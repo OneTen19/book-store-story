2a0ab3213c4eb4ef700ed54676f5055886b133a4
OneTen19 <khy000426@naver.com>
2024-08-02 09:51:19 +0900

    Initial Commit

---
+++ BookStoreStory/BookStoreStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreStoryApp.swift
+//  BookStoreStory
+//
+//  Created by OneTen on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
