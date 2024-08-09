8ad7b3bd61882ca7bb30022dfeef1a455b3d8687
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-08-09 14:59:32 +0900

    [feat] 회원가입 화면 스케치 3 - 회원 가입 완료 화면 (#21)

--- BookStoreStory/Presentation/SignUp/View/SignUpCompleteView.swift
+++ BookStoreStory/Presentation/SignUp/View/SignUpCompleteView.swift
@@ -8,23 +8,58 @@
 import SwiftUI
 
 struct SignUpCompleteView: View {
-
+    
     var body: some View {
         
+        VStack{
+            Spacer()
+            
+            VStack(alignment: .center){
+                Image(systemName: "checkmark.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100)
+                    .foregroundStyle(.brown)
+                    .padding()
+                
+                Text("환영합니다!!")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                
+                VStack(spacing: 5){
+                    
+                    Text("책방이야기 가입")
+                        .bold()
+                        .foregroundStyle(.brown)
+                        .font(.title2)
+                    +
+                    Text("이 완료되었습니다.")
+                        .font(.title3)
 
-        NavigationLink {
-            LoginView()
-        } label: {
-            // 회원가입 종료
-            Text("다음")
-                .foregroundStyle(.white)
-                .bold()
-                .padding(EdgeInsets(top: 15, leading: 150, bottom: 15, trailing: 150))
-                .background(.brown)
-                .clipShape(.buttonBorder)
+                    
+                    Text("로그인 후 이용해주세요!!")
+                        .font(.title3)
+                }
+
+                
+            }
+            
+            Spacer()
+            
+            NavigationLink {
+                LoginView()
+            } label: {
+                // 회원가입 종료
+                Text("확인")
+                    .foregroundStyle(.white)
+                    .bold()
+                    .padding(EdgeInsets(top: 15, leading: 150, bottom: 15, trailing: 150))
+                    .background(.brown)
+                    .clipShape(.buttonBorder)
+            }
         }
-        
-
+        .navigationBarBackButtonHidden()
     }
 }
 
