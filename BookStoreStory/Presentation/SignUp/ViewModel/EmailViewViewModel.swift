6ad7b2d0a63826039af6a1b14505487a1d62a7a8
OneTen19 <khy000426@naver.com>
2024-08-12 16:43:58 +0900

    [feat] 로그인&회원가입 이메일&패스워드 유효성 체크
    - Login View 와 Email View 까지 완료

--- BookStoreStory/Presentation/SignUp/ViewModel/EmailViewViewModel.swift
+++ BookStoreStory/Presentation/SignUp/ViewModel/EmailViewViewModel.swift
@@ -16,14 +16,49 @@ class EmailViewViewModel: ObservableObject {
     @Published var passwordCheck: String = ""
     @Published var passwordToggle: Bool = false
     @Published var isActive: Bool = false
+    @Published var errorMessage = ""
     
     init() {}
     
-    
     func next() {
-        //
+        guard validate() else {return}
+        
+        nextButtonTapped = true
+        
     }
     
+    private func validate() -> Bool {
+        errorMessage = ""
+        
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty,
+              !passwordCheck.trimmingCharacters(in: .whitespaces).isEmpty,
+              !name.trimmingCharacters(in: .whitespaces).isEmpty
+        else {
+            
+            errorMessage = "Please fill in all fields"
+            
+            return false
+        }
+        
+        
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "올바른 이메일 형식을 지켜주세요"
+            
+            return false
+        }
+        
+        
+        guard password == passwordCheck else {
+            errorMessage = "비밀번호를 다시 확인해주세요"
+            
+            return false
+        }
+        
+        
+        
+        return true
+    }
     
     
     
