92c4c324df49bcfe7cb9b5bcb6663b88f9524a43
OneTen19 <khy000426@naver.com>
2024-08-13 14:27:55 +0900

    [add] Firebase 연동

--- BookStoreStory/Other/BookStoreStoryApp.swift
+++ BookStoreStory/Other/BookStoreStoryApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct BookStoreStoryApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
