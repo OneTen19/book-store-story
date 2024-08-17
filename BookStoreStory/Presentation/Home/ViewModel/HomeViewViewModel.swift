976ff71c9d41dbd6c0c1feda5c497fcd62ce0cb8
김한열 Hanyeol Kim <63261054+OneTen19@users.noreply.github.com>
2024-08-17 20:29:20 +0900

    [fix] delete를 하면 유저는 삭제되는데 유저 정보는 firestore에 그대로 남아있음 (#38)

--- BookStoreStory/Presentation/Home/ViewModel/HomeViewViewModel.swift
+++ BookStoreStory/Presentation/Home/ViewModel/HomeViewViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class HomeViewViewModel: ObservableObject {
     
@@ -23,19 +24,34 @@ class HomeViewViewModel: ObservableObject {
     
     // 회원 탈퇴
     func deleteUser() {
-        Auth.auth().currentUser?.delete()
         
-//        if let user = Auth.auth().currentUser {
-//            user.delete { error in
-//                if let error = error {
-//                    print("Firebase Error : ",error)
-//                } else {
-//                    print("회원탈퇴 성공!")
-//                }
-//            }
-//        } else {
-//            print("로그인 정보가 존재하지 않습니다")
-//        }
+        // 둘이 왜 따로따로 구분 되는 지는 잘 모르겠음. 추후 개선해야 할 부분
+        if let user = Auth.auth().currentUser {
+            
+            // firestore database 에서 삭제
+            
+            let db = Firestore.firestore()
+            
+            db.collection("users")
+                .document(user.uid)
+                .delete()
+            
+            
+            // authentication 에서 삭제
+            user.delete { error in
+                if let error = error {
+                    print("Firebase Error : ",error)
+                } else {
+                    print("회원탈퇴 성공!")
+                }
+            }
+            
+            
+        } else {
+            print("로그인 정보가 존재하지 않습니다")
+        }
+        
+
         
     }
     
